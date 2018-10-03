00867da7e49f12f8e2fb6e0f9569f9b2a6201923
Russell Stewart <russells98@gmail.com>
2018-10-02 21:29:31 -0400

    add some comments

--- meltingtemp.py
+++ meltingtemp.py
@@ -1,6 +1,10 @@
+#Define sequences of forward and reverse primers as given in lab manual
 forward_primer = 'CCCCTGAATTCCATGGCACCAAAAGCAAAAATCGT'
 reverse_primer = 'CCCCTGGATCCTTAAGCTAATGCCTTCATTCTCT'
 
+#Start the Tm of the forward primer at zero. Iterate over each base in the
+#sequence of the forward primer and add 4 for each occurance of G or C, or 2
+#for each occurance of T or A. Print the result.
 forward_primer_Tm = 0
 for character in forward_primer:
     if character is 'G' or character is 'C':
@@ -9,6 +13,7 @@ for character in forward_primer:
         forward_primer_Tm += 2
 print 'The Tm of the forward primer is %d degrees celsius' % forward_primer_Tm
 
+#Repeat the same algorithm for the reverse primer.
 reverse_primer_Tm = 0
 for character in reverse_primer:
     if character is 'G' or character is 'C':
